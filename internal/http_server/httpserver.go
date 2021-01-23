95599fcf1baaa241d02fda2c90b762be951e759f
yumememooo <yumememooo@gmil.com>
2021-01-23 14:14:40 +0800

    Add: CORS for 前端 :beers:

--- internal/http_server/httpserver.go
+++ internal/http_server/httpserver.go
@@ -21,7 +21,7 @@ func StartHttpServer(errChan chan error) {
 
 	engine := gin.New()
 	engine.Use(ginLogHandler(), gin.Recovery())
-
+	engine.Use(Cors())
 	timeout := time.Duration(config.Configuration.Service.Timeout) * time.Millisecond
 	engine.Use(timeoutHandler(timeout))
 
@@ -67,6 +67,40 @@ func timeoutHandler(timeout time.Duration) gin.HandlerFunc {
 		}()
 
 		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin") //請求頭部
+		if origin != "" {
+			//接收客戶端傳送的origin （重要！）
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			//伺服器支援的所有跨域請求的方法
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+			//允許跨域設定可以返回其他子段，可以自定義欄位
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session")
+			// 允許瀏覽器（客戶端）可以解析的頭部 （重要）
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+			//設定快取時間
+			c.Header("Access-Control-Max-Age", "172800")
+			//允許客戶端傳遞校驗資訊比如 cookie (重要)
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		//允許型別校驗
+		if method == "OPTIONS" {
+			c.JSON(http.StatusOK, "ok!")
+		}
+
+		defer func() {
+			if err := recover(); err != nil {
+				log.SugarLogger.Errorf("Panic info is: %v", err)
+			}
+		}()
+
 		c.Next()
 	}
 }
